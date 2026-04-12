2fe32720156832022fe7d0876960b0f0c31dad87
Lawrence Li <lawrenceli3202@gmail.com>
2026-04-11 23:11:36 -0400

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res = 0
+        slow_car = 0
+
+        for p, s in sorted(zip(position, speed), reverse=True):
+            eta = (target - p) / s
+            if eta > slow_car:
+                res += 1
+                slow_car = eta
+        return res
